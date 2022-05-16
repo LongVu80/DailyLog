6a00a71adbb2f4581904ec995c87837b42a236b7
LongVu80 <thienlong14th@gmail.com>
2022-05-16 17:53:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+let reports = [];
+
+const filters = {
+    searchText: ''
+}
+
+const reportJSON = localStorage.getItem('reports');
+if (reportJSON !== null){
+    reports = JSON.parse(reportJSON)
+}
+
+const renderReports = function(reports, filters){
+    const filterReports = reports.filter(function(report){
+        return report.Time.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    document.querySelector('#log').innerHTML = '';
+    
+    // const summary = document.createElement('p');
+    // summary.textContent = `Wrote on ${d}`
+    // document.querySelector('#log').appendChild(summary);
+
+    filterReports.forEach(function(report){
+        // const d = new Date()
+    //     const summary = document.createElement('p');
+    // summary.textContent = `Wrote on: ${d}`
+    // document.querySelector('#log').appendChild(summary)
+    // localStorage.setItem('date', JSON.stringify(d))
+        const p = document.createElement('p');
+        //   ${d} <br> <br>
+        p.innerHTML = `<div id="result">Wrote on: ${report.Time} <br> <br> Shift: ${report.Shift}, Period: ${report.Period}, Duty: ${report.Duty} <br> Log: ${report.Report}</div>`
+        document.querySelector('#log').appendChild(p)
+    })
+}
+
+renderReports(reports, filters)
+
+// const time = function(){
+//     const d = new Date()
+//         const summary = document.createElement('p');
+//     summary.innerHTML = `<div id="time" value="${d}">${d}</div>`
+//     document.querySelector('#logTime').appendChild(summary)
+    
+// }
+
+document.querySelector('#new-report').addEventListener('submit', function (e) {
+    e.preventDefault()
+    const d = new Date()
+        const summary = document.createElement('p');
+    summary.textContent = `Wrote on:${d}`
+    document.querySelector('#logTime').appendChild(summary)
+    reports.push({
+        Time: d,
+        Shift: e.target.elements.shift.value,
+        Period: e.target.elements.period.value,
+        Duty: e.target.elements.duty.value,
+        Report: e.target.elements.report.value,
+        
+    })
+    localStorage.setItem('reports', JSON.stringify(reports))
+    renderReports(reports, filters)
+    e.target.elements.report.value = ''
+    setTimeout(function () {
+        location.reload()
+    }, 2000);
+})
+
+// setTimeout(function () {
+//         location.reload()
+//     }, 1000);
+
+const reload = function(){
+    location.reload();
+}
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderReports(reports, filters)
+})
+
+// $("#search-text").on("keyup", function(){
+//     let value = $(this).val();
+//     $("#log").each(function(records){
+//       if(records !== -1){
+//         let id=$(this).find("#Time${d}").text();
+//       if(id.indexOf(value)!==0 && id.toLowerCase().indexOf(value.toLowerCase())<0){
+//         $(this).hide();
+//       } else {
+//         $(this).show();
+//       }
+//       }
+      
+//     })
+//   })
